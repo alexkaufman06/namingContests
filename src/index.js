3a1f2053bdbbf504e747dce085b4a06dd4934d97
Alex Kaufman <alexkaufman06@gmail.com>
2017-07-04 18:34:42 -0700

    Extract components into separate files

--- src/index.js
+++ src/index.js
@@ -1,44 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-//-----HEADER COMPONENT CALLED IN APP COMPONENT-----
-const Header = ({ message }) => {
-	return (
-		<h2 className="Header text-center">
-			{message}
-		</h2>
-	);
-};
-
-Header.propTypes = {
-	message: PropTypes.string
-};
-
-const App = () => {
-	return (
-		<div>
-			<Header message="Naming Contests" />
-			<div>
-				...
-			</div>
-		</div>
-	);
-};
+import App from './components/App';
 
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
 );
 
-
-
-
 // cd Desktop
 // cd Root  
 // cd Programming\ Projects/
 // cd namingContests
-
 
 // npm run dev
 // npm start
